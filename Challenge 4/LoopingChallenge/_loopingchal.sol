aedb96edf51f780c4728e0d3595b2e30a02519d5
Josh <61985305+gunbullet02@users.noreply.github.com>
2023-02-14 17:47:12 +0800

    Create _loopingchal.sol
    Loop challenge

---
+++ Challenge 4/LoopingChallenge/_loopingchal.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+// program for trying out loops with complex 
+//and simple operations
+
+
+
+
+/*
+contract Loop{
+    uint public count;
+    
+    function loop(uint n) public{
+        for(uint i = 0; i < n; i++){
+            count += 1;
+        }
+    }
+}
+*/
+
+contract dividend{
+
+    address[500] public stakeholders;
+
+    function compensate() public{
+        for(uint i = 0; i < stakeholders.length; i++){
+
+        }
+
+    }
+}
